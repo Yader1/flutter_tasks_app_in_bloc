b44580dfc80dd7a7a919ef2bb6ea172e696b4bfe
Yader Campbell <yadermejiac123@gmail.com>
2023-08-22 08:16:33 -0600

    feat: Mark as Favorite

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../blocs/bloc_exports.dart';
 import '../models/task.dart';
@@ -19,7 +20,67 @@ class TaskTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                const Icon(Icons.star_outline),
+                const SizedBox(width: 10.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        task.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 18.0, decoration: task.isDone! ? TextDecoration.lineThrough : null)
+                      ),
+                      Text(DateFormat().add_yMEd().add_Hms().format(DateTime.now())),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Checkbox(
+                value: task.isDone,
+                onChanged: task.isDeleted == false ? (value){
+                  context.read<TasksBloc>().add(UpdateTask(task: task));
+                } : null,
+              ),
+              PopupMenuButton(itemBuilder: ((context)=>[
+                PopupMenuItem(child: TextButton.icon(
+                  onPressed: null,
+                  icon: const Icon(Icons.edit),
+                  label: const Text('Edit'),
+                ), onTap: (){}),
+                PopupMenuItem(child: TextButton.icon(
+                  onPressed: null,
+                  icon: const Icon(Icons.bookmark),
+                  label: const Text('Add to Booknarks'),
+                ), onTap: (){}),
+                PopupMenuItem(child: TextButton.icon(
+                  onPressed: null,
+                  icon: const Icon(Icons.delete),
+                  label: const Text('Delete'),
+                ), onTap: () => _removeOrDeleteTask(context, task))
+              ]))
+            ],
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+/*
+ListTile(
       title: Text(task.title, overflow: TextOverflow.ellipsis, style: TextStyle(
         decoration: task.isDone! ? TextDecoration.lineThrough : null
       )),
@@ -32,5 +93,4 @@ class TaskTile extends StatelessWidget {
       onLongPress: () => _removeOrDeleteTask(context, task),
 
     );
-  }
-}
+*/
